ab41bc7bcf374945acbee34bf01d538da60f00c1
Japheth Ramirez <japheth.ra369@gmail.com>
2024-04-21 15:29:42 -0600

    Cambios menores

--- back/src/controllers/getUsers.js
+++ back/src/controllers/getUsers.js
@@ -1,9 +1,7 @@
 const { User, Notification, Category, Record } = require("../db");
 
-module.exports = async function getUsers(id){
+module.exports = async function getUsers(){
   try {
-    if(!id){
-
       const users = await User.findAll({
         include: [
           { model: Notification },
@@ -33,37 +31,6 @@ module.exports = async function getUsers(id){
         }
       })
       return clearData.length > 0 ? clearData : "No hay Usuarios";
-
-    } else {
-
-      const user = await User.findOne({
-        where : {id : parseInt(id)}, 
-        include : [
-          {model : Notification},
-          {model : Category}
-        ]
-      })
-      const clearData = {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        notifications: user.Notifications?.map((element) => {
-          return {
-            id: element.id,
-            name: element.name,
-          };
-        }),
-        categories: user.Categories?.map((element) => {
-          return {
-            id: element.id,
-            name: element.name,
-          };
-        }),
-      };
-      return clearData ? clearData : "No existe ese usuario";
-    }
-
   } catch (error) {
     console.log(error)
     return error
